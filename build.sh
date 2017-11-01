fff578d43e5c95cd210a9f3570dd729179c837ba
hishammuddin.sani <comradesharf@gmail.com>
2017-11-01 18:48:46 +0800

    * Add publish script * Publish version 1.0.0

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 set -u -e -o pipefail
 
 export PYTHONIOENCODING=utf8;
+CLEAN=false;
+INSTALL=false;
+BUILD=false;
+PUBLISH=false;
 
 ##################
 # Get value in json string given key.
@@ -21,18 +25,19 @@ jsonExtractValue() {
 # Arguments:
 #   param1 - yarn command
 #   param2 - text to show
-#   param3 - absolute path to the directory containing package.json
+#   param3 - subdir containing packages
+#   param4 - absolute path to the directory containing package.json
 # Returns:
 #   None
 ##################
 scripts() {
-    pushd $3 > /dev/null;
+    pushd "$4" > /dev/null;
     packageName=$(jsonExtractValue './package.json' 'name');
     echo "=== $2 ${packageName}"
     yarn $1 > /dev/null 2>&1 || true;
-    for package in packages/* ; do
+    for package in $3/* ; do
         if [[ -d "$(pwd)/${package}" && -f "$(pwd)/${package}/package.json" ]]; then
-            scripts $1 $2 "$(pwd)/${package}"
+            scripts "$1" "$2" "$3" "$(pwd)/${package}"
             popd > /dev/null;
         fi
     done
@@ -46,7 +51,7 @@ scripts() {
 #   None
 ##################
 install() {
-    scripts 'install' 'Installing' $1;
+    scripts 'install' 'Installing' 'packages' $1;
 }
 
 ##################
@@ -57,7 +62,7 @@ install() {
 #   None
 ##################
 build() {
-    scripts 'build' 'Building' $1;
+    scripts 'build' 'Building' 'packages' $1;
 }
 
 ##################
@@ -68,9 +73,49 @@ build() {
 #   None
 ##################
 clean() {
-    scripts 'clean' 'Cleaning' $1;
+    scripts 'clean' 'Cleaning' 'packages' $1;
 }
 
-clean $(pwd);
-install $(pwd);
-build $(pwd);
+publish() {
+    scripts 'publish --new-version 1.0.0 --access public' 'Publishing' 'dist' $1;
+}
+
+for ARG in "$@"; do
+  case "$ARG" in
+    --clean)
+      CLEAN=true;
+      ;;
+    --install)
+      INSTALL=true;
+      ;;
+    --build)
+      INSTALL=true;
+      BUILD=true;
+      ;;
+    --publish)
+      INSTALL=true;
+      BUILD=true;
+      PUBLISH=true;
+      ;;
+    *)
+      echo "Unknown option $ARG."
+      exit 1
+      ;;
+  esac
+done
+
+if [[ ${CLEAN} == true ]]; then
+    clean $(pwd);
+fi
+
+if [[ ${INSTALL} == true ]]; then
+    install $(pwd);
+fi
+
+if [[ ${BUILD} == true ]]; then
+    build $(pwd);
+fi
+
+if [[ ${PUBLISH} == true ]]; then
+    publish $(pwd);
+fi
